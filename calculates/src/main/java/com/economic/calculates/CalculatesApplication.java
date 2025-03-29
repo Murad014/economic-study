7e18c5749547c00cf2be5dc7c3603e33e16e9938
muradguliyev <quliyev.murad@yahoo.com>
2025-03-29 22:59:15 +0400

    Initialize the project

---
+++ calculates/src/main/java/com/economic/calculates/CalculatesApplication.java
@@ -0,0 +1,13 @@
+package com.economic.calculates;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculatesApplication.class, args);
+	}
+
+}
---
+++ calculates/src/test/java/com/economic/calculates/CalculatesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.economic.calculates;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
